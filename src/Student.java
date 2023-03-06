5b42d35c5c1727928a917f1bdafb7f7631ce54b6
jurkobrillka <brillaju@gmail.com>
2023-03-06 22:36:35 +0100

    Test GIT

--- src/Student.java
+++ src/Student.java
@@ -1,4 +1,12 @@
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+
 public class Student {
+
+
+
+
 
     private int ID;
     //moze byt ID velkym alebo ma byt ako premenna malym?
@@ -51,4 +59,86 @@ public class Student {
         this.age = age;
     }
 
+    @Override
+    public String toString() {
+        return "Student{" +
+                "ID=" + ID +
+                ", name='" + name + '\'' +
+                ", surname='" + surname + '\'' +
+                ", age=" + age +
+                '}';
+    }
+
+    public void createStudents(){
+        String randomLetters = "dcblachdsnxslhdfvilykhcvsd";
+        for (int i = 0; i < Main.students.length ; i++) {
+            Student s = new Student(i,randomLetters.charAt(i)+"xyz",randomLetters.charAt(i+2)+"abc",(i+32));
+            Main.students[i] = s;
+        }
+    }
+
+    public void sortingStudents(){
+        System.out.println("Zadaj parameter podla ktoreho chces sortovat: (id, name, surname, age)");
+        String choiceMenu = Main.sc.nextLine();
+        System.out.println("Zadaj asc/ desc (0/1)");
+        int ascChoice = Main.sc.nextInt();
+
+
+        for (Student s: sortedArr(Main.students,choiceMenu,ascChoice)){
+            System.out.println(s.toString());
+        }
+    }
+
+    public Student[] sortedArr(Student[] students, String whatToSort, int asc){
+        switch(whatToSort.toLowerCase()){
+            case "id":
+                Arrays.sort(students, new Comparator<Student>() {
+                    @Override
+                    public int compare(Student o1, Student o2) {
+                        return o1.getID() - o2.getID();
+                    }
+                });
+                break;
+
+            case "name":
+                //sortByName
+                System.out.println("turuuru");
+                Arrays.sort(students, new Comparator<Student>() {
+                    @Override
+                    public int compare(Student o1, Student o2) {
+
+                        return o1.getName().compareTo(o2.getName());
+                    }
+                });
+                break;
+
+            case "surname":
+                Arrays.sort(students, new Comparator<Student>() {
+                    @Override
+                    public int compare(Student o1, Student o2) {
+                        return o1.getSurname().compareTo(o2.getSurname());
+                    }
+                });
+                break;
+
+            case "age":
+                Arrays.sort(students, new Comparator<Student>() {
+                    @Override
+                    public int compare(Student o1, Student o2) {
+                        return o1.getAge() - o2.getAge();
+                    }
+                });
+                break;
+
+        }
+
+
+        if (asc == 0) {
+            Collections.reverse(Arrays.asList(students));
+        } else {
+            Collections.reverse(Arrays.asList(students));
+        }
+
+        return students;
+    }
 }
